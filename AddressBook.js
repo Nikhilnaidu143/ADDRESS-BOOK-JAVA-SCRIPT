16f6358968864eff5f9d343a92588c06a5f2d151
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-31 07:38:41 +0530

    UC-4:- Edit existing contact feature is added.

--- AddressBook.js
+++ AddressBook.js
@@ -2,8 +2,8 @@ console.log("---------WELCOME TO ADDRESS BOOK PROBLEM USING JAVA-SCRIPT---------
 
 class Contact{
     /** Variables. */
-    first_name;
-    last_name;
+    firstName;
+    lastName;
     address;
     city;
     state;
@@ -12,9 +12,9 @@ class Contact{
     email;
 
     /** Constructor */
-    constructor(first_name , last_name , address , city , state , zip_code , phone_number , email){
-        this.first_name = first_name;
-        this.last_name = last_name;
+    constructor(firstName , lastName , address , city , state , zip_code , phone_number , email){
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.address = address;
         this.city = city;
         this.state = state;
@@ -59,15 +59,46 @@ function addContacts(firstName , lastName , address , city , state , zip_code , 
     }
 }
 
+/** Editing contacts by using first name. */
+function editContact(first_name , option , newValue){
+    addressBook.forEach(contact => {
+        if(contact.firstName == first_name){  //edit contact if contact with firstname was found in the address book.
+            switch(option){
+                case "firstName":
+                    contact.firstName = newValue;
+                    break;
+                case "lastName":
+                    contact.lastName = newValue;
+                    break;
+                case "address":
+                    contact.address - newValue;
+                    break;
+                case "city":
+                    contact.city = newValue;
+                    break;
+                case "state":
+                    contact.state = newValue;
+                    break;                    case "zipCode":
+                    contact.zip_code = newValue;
+                    break;
+                case "phoneNum":
+                    contact.phone_number = newValue;
+                    break;
+                case "email":
+                    contact.email = newValue;
+                    break;
+                default:
+                    console.log("Please choose valid option.");
+            }
+        }
+    });
+}
+
 /** Calling function by passing parameters. */
 addContacts("Nikhil","Sundarasetty","Pellur","Ongole","Andhra pradesh",523272,"91 9398359637","nikhilnaidu473@gmail.com");
 addContacts("Bill","Gates","Newyork","America","America","400 008","91 9258566655","bill-gates@gmail.com");
+console.log(addressBook);
 
-/** Displaying contacts in the address book array. */
-function displayContacts(){
-    for(let i = 0; i < addressBook.length; i++){
-        console.log(addressBook[i]);
-    }
-}
-
-displayContacts(); //calling function to print array.
+/** Calling function by passing parameters for edit. */
+editContact("Nikhil","state","Andhra");
+console.log(addressBook);
