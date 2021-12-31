6494c1b71c335f9c7db6058144f99366c9381348
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-31 06:04:19 +0530

    UC-2:- Validating Contact details.

--- AddressBook.js
+++ AddressBook.js
@@ -25,24 +25,24 @@ class Contact{
 }
 
 /** Regex patterns. */
-let pattern_name = /^[A-Z][A-Z a-z]{3,}$/;
-let pattern_address = /^[A-Z][A-Z a-z]{4,}$/;
-let pattern_city = /^[A-Z][A-Z a-z]{4,}$/;
-let pattern_state = /^[A-Z][A-Z a-z]{4,}$/;
-let pattern_zip = /^[\d]{3}[\s]?[\d]{3}$/;
-let pattern_phoneNum = /^[\d]{2}(\s){1}[\d]{10}$/;
-let pattern_email = /^[\w+-]+(\.[\w+-]+)*@[\w]+(\.[\w]+)?(?=(\.[A-Za-z_]{2,3}$|\.[a-zA-Z]{2,3}$)).*$/;
+const PATTERN_NAME = /^[A-Z][A-Z a-z]{3,}$/;
+const PATTERN_ADDRESS = /^[A-Z][A-Z a-z]{4,}$/;
+const PATTERN_CITY = /^[A-Z][A-Z a-z]{4,}$/;
+const PATTERN_STATE = /^[A-Z][A-Z a-z]{4,}$/;
+const PATTERN_ZIP = /^[\d]{3}[\s]?[\d]{3}$/;
+const PATTERN_PHONE = /^[\d]{2}(\s){1}[\d]{10}$/;
+const PATTERN_EMAIL = /^[\w+-]+(\.[\w+-]+)*@[\w]+(\.[\w]+)?(?=(\.[A-Za-z_]{2,3}$|\.[a-zA-Z]{2,3}$)).*$/;
 
 /** Adding contacts when validated with regex patterns. */
 function addContacts(firstName , lastName , address , city , state , zip_code , phone_number , email){ 
-    let result_firstName = pattern_name.test(firstName); 
-    let result_lastName = pattern_name.test(lastName);
-    let result_address = pattern_address.test(address);
-    let result_city = pattern_city.test(city);
-    let result_state = pattern_state.test(state);
-    let result_zip = pattern_zip.test(zip_code);
-    let result_phoneNum = pattern_phoneNum.test(phone_number);
-    let result_email = pattern_email.test(email);
+    let result_firstName = PATTERN_NAME.test(firstName); 
+    let result_lastName = PATTERN_NAME.test(lastName);
+    let result_address = PATTERN_ADDRESS.test(address);
+    let result_city = PATTERN_CITY.test(city);
+    let result_state = PATTERN_STATE.test(state);
+    let result_zip = PATTERN_ZIP.test(zip_code);
+    let result_phoneNum = PATTERN_PHONE.test(phone_number);
+    let result_email = PATTERN_EMAIL.test(email);
 
     if(result_firstName == true && result_lastName == true && result_address == true && result_city == true && 
        result_state == true && result_zip == true && result_phoneNum == true && result_email == true){
